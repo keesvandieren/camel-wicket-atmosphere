2b24059572072402d054fc4fa2acd50813148282
keesvandieren <keesvandieren@gmail.com>
2013-07-30 08:01:33 +0200

    minor improvement.

--- cawa-common/src/main/java/com/squins/cawa/CawaRouteBuilder.java
+++ cawa-common/src/main/java/com/squins/cawa/CawaRouteBuilder.java
@@ -5,7 +5,7 @@ import org.apache.camel.builder.RouteBuilder;
 public class CawaRouteBuilder extends RouteBuilder {
 
     public void configure() {
-        String vmEndpoint = "vm:sampleVmQueue?concurrentConsumers=2&multipleConsumers=true";
+        String vmEndpoint = "vm:sampleVmQueue?concurrentConsumers=4&multipleConsumers=true";
         from("direct:start").multicast().to(vmEndpoint);
 
         from(vmEndpoint).beanRef("sampleConsumer");
--- cawa-common/src/main/java/com/squins/cawa/SampleConsumer2.java
+++ cawa-common/src/main/java/com/squins/cawa/SampleConsumer2.java
@@ -7,20 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ServletContextAware;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kees
- * Date: 7/29/13
- * Time: 10:45 PM
- * To change this template use File | Settings | File Templates.
- */
 public class SampleConsumer2 implements ServletContextAware {
 
     private static final Logger LOG = LoggerFactory.getLogger(SampleConsumer2.class);
 
     private String name;
 
-    @Consume(uri = "vm:sampleVmQueue?concurrentConsumers=2&multipleConsumers=true")
+    @Consume(uri = "vm:sampleVmQueue?concurrentConsumers=4&multipleConsumers=true")
     public void consume(String message) {
         LOG.warn("Sample consumer 2 " + name + " just received:" + message);
     }
